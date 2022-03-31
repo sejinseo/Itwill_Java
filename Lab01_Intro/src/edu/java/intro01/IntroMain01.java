e63b0564b1eff77c99f48ea743df561aad27e348
sejinseo <tjtpwls1@naver.com>
2022-03-31 10:35:06 +0900

    .

---
+++ Lab01_Intro/src/edu/java/intro01/IntroMain01.java
@@ -0,0 +1,20 @@
+package edu.java.intro01;
+
+//주석(comment) - 설명
+/* 문서화 주석(documentation comment)
+*
+*/
+
+public class IntroMain01 {
+
+	// main 메서드(method): 프로그램의 시작점.
+	// main 메서드가 끝나면 프로그램이 종료됨.
+	public static void main(String[] args) {
+		// 콘솔에 문자열을 출력.
+		System.out.println("안녕하세요.");
+		System.out.println("Hello, Java");
+		// 모든 문장의 끝은 ;
+		// 실행(Run) 단축키: Ctrl+F11
+	}
+
+}
---
+++ Lab01_Intro/src/edu/java/intro02/CalcMain.java
@@ -0,0 +1,19 @@
+package edu.java.intro02;
+
+public class CalcMain {  // 클래스 시작
+	// main 메서드(기능)
+	public static void main(String[] args) {  // main 메서드 시작
+		System.out.println(100 + 200);  // 100 + 200 계산 결과를 출력
+		System.out.println(100 * 200);  // 100 x 200 계산 결과를 출력
+		System.out.println(10 / 2);  // 10 / 2 계산 결과를 출력
+		
+		// 위 결과와 비교
+		System.out.println("100 + 200");  // 문자열(string)을 출력
+		// 문자열(string): 문자들의 집합(sequence). 큰따옴표("")로 묶음.
+		// 문자(character): 문자 (1개). 작은따옴표('')로 묶음.
+		System.out.println('a');  // a 문자 (1개)를 콘솔에 출력.
+		System.out.println("a");  // 문자 1개로 이루어진 문자열을 콘솔에 출력.
+		
+	}  // main 메서드 끝
+	
+}  // 클래스 끝
